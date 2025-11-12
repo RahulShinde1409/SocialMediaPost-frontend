4fb8c7d133fa38d8e4bb8febeba0a10b50980f3f
Rahul <rahulshinde8828@gmail.com>
2025-11-12 13:14:48 +0530

    vercel

--- store/action/createpost.action.js
+++ store/action/createpost.action.js
@@ -1,50 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axioshttp from "../../utils/axiosInterceptor";
 
-// export const createPost = createAsyncThunk(
-//   'posts/createPost',
-//   async (formData, { rejectWithValue }) => {
-//     try {
-//       const token = localStorage.getItem("token");
-//       const response = await axioshttp.post('/add-post', formData, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data',
-//           Authorization: `Bearer ${token}`,
-//         },
-
-//       });
-//       console.log(response.data)
-//       return response.data;
-//     } catch (err) {
-//       return rejectWithValue(err.response?.data?.message || "Post Create Failed");
-//     }
-//   }
-// );
-
-
 export const createPost = createAsyncThunk(
-  "posts/createPost",
+  'posts/createPost',
   async (formData, { rejectWithValue }) => {
     try {
       const token = localStorage.getItem("token");
-
-      const response = await axioshttp.post("/add-post", formData, {
+      const response = await axioshttp.post('/add-post', formData, {
         headers: {
-          "Content-Type": "multipart/form-data",
+          'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${token}`,
         },
-      });
 
-      console.log("✅ Post created successfully:", response.data);
+      });
+      console.log(response.data)
       return response.data;
-    } catch (error) {
-      console.error("❌ Post creation failed:", error.response || error);
-      return rejectWithValue(
-        error.response?.data?.message || "Failed to create post"
-      );
+    } catch (err) {
+      return rejectWithValue(err.response?.data?.message || "Post Create Failed");
     }
   }
 );
+
 export const UserPosts = createAsyncThunk(
   "posts/fetchUserPosts",
   async (userId, thunkAPI) => {
