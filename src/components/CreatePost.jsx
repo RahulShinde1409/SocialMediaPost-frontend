df8becc6c72958cff1e5f7e0048ed13e5a94e6a5
Rahul <rahulshinde8828@gmail.com>
2026-07-20 13:28:30 +0530

    CreatePost

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createPost } from '../../store/action/createpost.action';
+import { createPost, UserPosts} from '../../store/action/createpost.action';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import Header from './Header';
